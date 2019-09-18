f65c63fb4761a37e5cb3a5ca84c0118b6fe9e006
KChristelle <r.ayateke@alustudent.com>
2019-09-18 13:32:37 +0200

    Created flag of Congo

--- script_congo.py
+++ script_congo.py
@@ -5,9 +5,12 @@ window_length = 1000
 
 
 def congo():
-    set_fill_color(0, 0, 1)
-    triangle_length = window_length - 3
-    draw_triangle(x1=0, y1=0, x2=triangle_length, y2=window_height)
+    set_fill_color(0, 0.6, 0)
+    draw_triangle(x1=0, y1=0, x2=0, y2=window_height, x3=700, y3=0)
+    set_fill_color(1, 0, 0)
+    draw_triangle(x1=window_length, y1=window_height, x2=window_length, y2=0, x3=0, y3=window_length)
+    set_clear_color(0, 0.6, 0)
 
 
-start_graphics(congo(), width=window_length, height=window_height)
+
+start_graphics(congo, width=window_length, height=window_height)
